6ef6e16ce2e8ad6e3802165a1ee2f8731a8bca7e
abigailthomas <agthomas084@gmail.com>
2023-10-30 14:45:26 -0400

    Update main.py

--- main.py
+++ main.py
@@ -3,9 +3,23 @@ from pygame import *
 
 pygame.init()
 
-screen_width = 200
-screen_height = 500
+screen_width = 300
+screen_height = 600
+
+# variables
+run = True
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('ShapeScape')
 
+while run: 
+
+     # if user clicks exit window, game quits
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT:
+            run = False
+
+    
+    pygame.display.update()
+
+pygame.quit()
