813870661093b83003c38508aeeda9261246e728
abigailthomas <agthomas084@gmail.com>
2023-10-30 14:39:33 -0400

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,11 @@
-print("hello")
+import pygame
+from pygame import * 
+
+pygame.init()
+
+screen_width = 200
+screen_height = 500
+
+screen = pygame.display.set_mode((screen_width, screen_height))
+pygame.display.set_caption('ShapeScape')
+
