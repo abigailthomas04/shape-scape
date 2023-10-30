7cf869121107357531a551975d913a652447d30a
abigailthomas <agthomas084@gmail.com>
2023-10-30 15:13:35 -0400

    Update main.py
    scrolling the background

--- main.py
+++ main.py
@@ -8,16 +8,18 @@ run = True
 screen_width = 400
 screen_height = 600
 scroll = 0
-scroll_speed = 1
+scroll_speed = 0.05
 
 screen = pygame.display.set_mode((screen_width, screen_height))
 pygame.display.set_caption('ShapeScape')
 
 # upload images
+bg = pygame.image.load('shape-scape/img/blue_bg.jpg')
 sand = pygame.image.load('shape-scape/img/sand.jpg')
 seaweed = pygame.image.load('shape-scape/img/seaweed.png')
 
 # resize images
+bg = pygame.transform.scale(bg, (screen_width, screen_height))
 sand = pygame.transform.scale(sand, (screen_width, 50))
 seaweed = pygame.transform.scale(seaweed, (50, 50))
 
@@ -30,13 +32,22 @@ while run:
         if event.type == pygame.QUIT:
             run = False
 
+    # draw background
+    screen.blit(bg, (0, scroll))
+    screen.blit(bg, (0, scroll - 600))
+
     # draw sand
-    screen.blit(sand, (0, 550))
+    '''screen.blit(sand, (0, 550 + scroll))
     
     # draw seaweed
-    screen.blit(seaweed, (75, 515))
-    screen.blit(seaweed, (200, 505))
-    screen.blit(seaweed, (300, 520))
+    screen.blit(seaweed, (75, 550 + scroll))
+    screen.blit(seaweed, (200, 550 + scroll))
+    screen.blit(seaweed, (300, 550 + scroll))'''
+
+    # scroll the game
+    scroll += scroll_speed
+    if abs(scroll) > 600:
+        scroll = 0
     
     pygame.display.update()
 
